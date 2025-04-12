4ab2d6fadb8d5de110b4f0798bb853b8b4c81937
Олександр Шаповалов web2025-4 <alexandrshapovalov11@gmail.com>
2025-04-12 14:20:50 +0300

    Initial commit: Added server with JSON to XML conversion

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Command } = require('commander');
 const fs = require('fs');
 const http = require('http');
 const path = require('path');
+const xml2js = require('xml2js'); 
 
 const program = new Command();
 
@@ -15,19 +16,34 @@ const options = program.opts();
 
 const inputPath = path.resolve(options.input);
 
-if (!fs.existsSync(inputPath)) {
-  console.error('Cannot find input file');
-  process.exit(1);
+async function readFile() {
+  try {
+    const data = await fs.promises.readFile(inputPath, 'utf-8');
+    return JSON.parse(data); // Парсимо JSON
+  } catch (error) {
+    console.error('Cannot find input file');
+    process.exit(1);
+  }
 }
 
-const data = fs.readFileSync(inputPath);
+async function convertJsonToXml(jsonData) {
+  const builder = new xml2js.Builder({ headless: true, renderOpts: { pretty: true } });
+  const xml = builder.buildObject(jsonData);
+  return xml;
+}
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(data);
+const server = http.createServer(async (req, res) => {
+  try {
+    const jsonData = await readFile(); // Читання JSON
+    const xmlData = await convertJsonToXml(jsonData); // Перетворення в XML
+    res.writeHead(200, { 'Content-Type': 'application/xml' });
+    res.end(xmlData); // Відправка XML
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Server Error');
+  }
 });
 
 server.listen(options.port, options.host, () => {
   console.log(`Server running at http://${options.host}:${options.port}`);
 });
-
